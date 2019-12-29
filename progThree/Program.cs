3ccdd19be925d450ba986740171c703a4b18e3da
Elijah <Superil2q@yandex.ru>
2019-12-29 13:20:13 +0300

    lab 3, start

---
+++ progThree/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program
+{
+    class Program
+    {
+        static double[] A = { -500.006, 499.955, 499.995, -500.005 };
+        static void Main(string[] args)
+        {
+            var auto = true;
+            
+            var N = 10000;
+            var x = 0;
+            var xmax = 800;
+            var h = 0.001;
+            var eps = 0.00001;
+        }
+
+        static double[] U(double x)
+        {
+            double[] a1 = { 10 * Math.Exp(-0.01 * x), 10 * Math.Exp(-0.01 * x) };
+            double[] a2 = { 3 * Math.Exp(-1000 * x), -3 * Math.Exp(-1000 * x) };
+
+            double[] res = { a1[0] - a2[0], a1[1] - a2[1] };
+
+            return res;
+        }
+
+        static double[] rk(double[] v, double h)
+        {
+            double[,] E = { { 1, 0 }, { 0, 1 } };
+
+            
+
+
+        }
+
+    }
+}
---
+++ progThree/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("program")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("program")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("45040eba-c67c-42d8-85d9-546e9bd510cb")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
